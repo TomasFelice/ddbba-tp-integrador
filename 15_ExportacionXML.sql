5d265ae7e8f713fd5041680f02b96e7801e847e9
IgnacioNogueira <ignacio.nogueira@hotmail.com>
2024-06-11 18:59:14 -0300

    ExportacionXML a probar

--- 15_ExportacionXML.sql
+++ 15_ExportacionXML.sql
@@ -80,34 +80,7 @@ go
 --Comandos para la exportación
 
 SELECT * FROM ObraSocial.Prestador
-go
+GO
+EXEC Paciente.ExportarTurnosXML 'Union Personal', '2005-01-15', '2009-03-12';
 
-drop procedure if exists Paciente.InsertarDatosIniciales -- esto sirve como filtro para poder exportar el xml (lo tenemos que ejecutar para poder exportar un xml)
-go
-CREATE PROCEDURE Paciente.InsertarDatosIniciales
-AS
-BEGIN
-IF NOT EXISTS (SELECT
-            P.nro_de_documento AS 'Paciente/DNI'
-            FROM Turno.ReservaTurnoMedico AS T
-            INNER JOIN Paciente.Paciente AS P ON T.id_historia_clinica = P.id_historia_clinica
-            INNER JOIN Hospital.Medico AS PR ON T.id_medico = PR.id_medico
-            INNER JOIN Hospital.MedicoEspecialidad AS ME ON T.id_medico_especialidad = ME.id_medico_especialidad
-            INNER JOIN Hospital.Especialidad AS E ON ME.id_especialidad = E.id_especialidad
-            INNER JOIN Turno.EstadoTurno AS ET ON T.id_estado_turno = ET.id_estado
-            INNER JOIN ObraSocial.Prestador AS OS ON T.id_prestador = OS.id_prestador)
-	begin
-	begin
-
-        exec Pacientes.InsertarCobertura 1,Null,NULL --Inserto cobertura con id prestador 1
-		update Pacientes.Paciente set paciente.ID_Cobertura=1 where Paciente.ID_HistoriaClinica=1  --actualizo paciente seteando cobertura=1
-		exec TurnosMedicos.InsertarReservaTurnoMedico 1,1,1,1,1,1,'2005-01-17',NULL --agrego un registro que cumpla con las condiciones del XML	
-	end
-end;
-go
-exec Paciente.InsertarDatosIniciales;
-go
-exec Paciente.ExportarTurnosXML 'Union Personal', '2005-01-15', '2009-03-12';
-go
-use tempdb;
 
