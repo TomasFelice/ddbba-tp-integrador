61ca501716c5cb6faa218991b3b48a0cf64307c5
Tomi <tomasagustin.200358@gmail.com>
2024-06-11 21:01:16 -0300

    fix orden archivos

--- 09_CreacionRoles.sql
+++ 09_CreacionRoles.sql
@@ -16,6 +16,36 @@
 USE com2900g09 
 GO
 
+IF EXISTS (SELECT * FROM sys.server_principals WHERE name = 'Paciente')
+DROP LOGIN Paciente;
+
+IF EXISTS (SELECT * FROM sys.server_principals WHERE name = 'Medico')
+DROP LOGIN Medico;
+
+IF EXISTS (SELECT * FROM sys.server_principals WHERE name = 'AdministradorGeneral')
+DROP LOGIN AdministradorGeneral;
+
+IF EXISTS (SELECT * FROM sys.server_principals WHERE name = 'TecnicoClinico')
+DROP LOGIN TecnicoClinico;
+
+IF EXISTS (SELECT * FROM sys.server_principals WHERE name = 'Administrativo')
+DROP LOGIN Administrativo;
+
+IF EXISTS (SELECT * FROM sys.database_principals WHERE name = 'Paciente')
+DROP USER Paciente;
+
+IF EXISTS (SELECT * FROM sys.database_principals WHERE name = 'Medico')
+DROP USER Medico;
+
+IF EXISTS (SELECT * FROM sys.database_principals WHERE name = 'AdministradorGeneral')
+DROP USER AdministradorGeneral;
+
+IF EXISTS (SELECT * FROM sys.database_principals WHERE name = 'TecnicoClinico')
+DROP USER TecnicoClinico;
+
+IF EXISTS (SELECT * FROM sys.database_principals WHERE name = 'Administrativo')
+DROP USER Administrativo;
+
 CREATE LOGIN Paciente WITH PASSWORD = 'BocaJuniors'
 CREATE LOGIN Medico WITH PASSWORD = 'BocaJuniors'
 CREATE LOGIN AdministradorGeneral WITH PASSWORD = 'BocaJuniors'
@@ -28,12 +58,12 @@ CREATE USER AdministradorGeneral FOR LOGIN AdministradorGeneral
 CREATE USER TecnicoClinico FOR LOGIN TecnicoClinico
 CREATE USER Administrativo FOR LOGIN Administrativo
 
-GRANT SELECT ON SCHEMA::Pacientes TO Paciente;
+GRANT SELECT ON SCHEMA::Paciente TO Paciente;
 GRANT CONTROL ON OBJECT::Turno.ReservaTurnoMedico TO Paciente;
 
 GRANT SELECT ON SCHEMA::Hospital TO Medico;
 
-GRANT CONTROL ON SCHEMA::Pacientes TO AdministradorGeneral;
+GRANT CONTROL ON SCHEMA::Paciente TO AdministradorGeneral;
 GRANT CONTROL ON SCHEMA::ObraSocial TO AdministradorGeneral;
 GRANT CONTROL ON SCHEMA::Hospital TO AdministradorGeneral;
 GRANT CONTROL ON SCHEMA::Turno TO AdministradorGeneral;
@@ -41,5 +71,5 @@ GRANT CONTROL ON SCHEMA::Turno TO AdministradorGeneral;
 GRANT CONTROL ON SCHEMA::Turno TO TecnicoClinico;
 GRANT CONTROL ON SCHEMA::Hospital TO TecnicoClinico;
 
-GRANT CONTROL ON SCHEMA::Pacientes TO Administrativo;
+GRANT CONTROL ON SCHEMA::Paciente TO Administrativo;
 GRANT CONTROL ON SCHEMA::Turno TO Administrativo;
